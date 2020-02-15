9d6de63a16fc6d507f93058920743c4c64f19fef
Chirag Jakhariya <chigu.jakhariya@gmail.com>
2020-02-15 15:14:01 +0530

    styled comp

--- src/components/store/store.js
+++ src/components/store/store.js
@@ -2,6 +2,12 @@ import { createStore } from "redux";
 import rootReducer from "../redux/root-reducer";
 import { persistStore } from "redux-persist";
 
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+    middlewares.push("j");
+}
+
 export const store = createStore(rootReducer);
 
 export const persistor = persistStore(store);
