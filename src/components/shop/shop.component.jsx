6702e3729ad3655cd6c421eb55d67e1487ba98c4
Shubhangi Ambade <shubhambade6897@gmail.com>
2020-02-20 12:21:09 +0530

    chnages

--- src/components/shop/shop.component.jsx
+++ src/components/shop/shop.component.jsx
@@ -2,14 +2,44 @@ import React from 'react';
 import CollectionOverview from '../collection-overview/collection-overview.component';
 import { Route } from 'react-router-dom';
 import Collection from '../../pages/collection/collection.component';
+import WithSpinner from '../with-spinner/with-spinner.component';
 
-const ShopPage = ({ match }) => {
-    return (
-        <div className="shop-page">
-            <Route component={CollectionOverview} exact path={`${match.path}`} />
-            <Route path={`${match.path}/:collectionId`} component={Collection} />
-        </div>
-    )
+const CollectionOverviewWithSpinner = WithSpinner(CollectionOverview);
+const CollectionPageWithSpinner = WithSpinner(Collection);
+
+class ShopPage extends React.Component {
+    state = {
+        loading: true
+    }
+
+    componentDidMount() {
+        const { updateCollections } = this.props;
+        console.log(this.props);
+        fetch('https://firestore.googleapis.com/v1/projects/crawn-db-4156d/databases/(default)/documents/users').then(res => res.json()).then(data => {
+            console.log("adas", data);
+        })
+        // const collectionRef = firebase.collection('collections');
+
+        // this.unsubscribeFromSnapShot = collectionRef.onSnapShot(async snapshot => {
+        //     updateCollections(collectionsMap);
+        //     // this.setState({ loading: false });
+        // })
+    }
+
+    render() {
+        const { match } = this.props;
+        const loading = this.state;
+        return (
+            <div className="shop-page">
+
+                <Route exact
+                    render={(props) => <CollectionOverviewWithSpinner isLoading={false} {...props} />}
+                    path={`${match.path}`} />
+                <Route path={`${match.path}/:collectionId`}
+                    render={(props) => <CollectionPageWithSpinner isLoading={false} {...props} />} />
+            </div>
+        )
+    }
 }
 
 export default ShopPage;
